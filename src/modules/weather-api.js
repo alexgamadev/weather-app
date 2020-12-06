12b6f749b7855f1c5892ad6b2a5468c9e0f70fb8
AlexGamaDev <alexgama.97@outlook.com>
2020-12-06 17:06:02 +0000

    Changed API call to HTTPS

--- src/modules/weather-api.js
+++ src/modules/weather-api.js
@@ -2,7 +2,7 @@ import lookup from 'country-code-lookup';
 
 export async function getWeatherData(city) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5dbd617e8b48907cb3780b57ff5f1ffa&units=metric`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5dbd617e8b48907cb3780b57ff5f1ffa&units=metric`, { mode: 'cors' });
     const data = await response.json();
     if (data.cod === '404') {
       return Promise.reject(new Error(data.message));
